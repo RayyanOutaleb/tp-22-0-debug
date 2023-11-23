3932a30f473df8dfacc6789a75207acb079404d6
RayyanOutaleb <Outalebr@3il.fr>
2023-11-23 14:48:04 +0100

    1er test

---
+++ tptest/FizzBuzz.java
@@ -0,0 +1,7 @@
+package tptest;
+
+public class FizzBuzz {
+	public static String FizzBuzz (int i) {
+		return "1";
+	}
+}
---
+++ tptest/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package tptest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+	@Test
+	public void test1() {
+		assertEquals(FizzBuzz.FizzBuzz(1), "1");
+	}
+
+}
